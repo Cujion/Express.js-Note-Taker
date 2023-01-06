3e62cadb7b4da7472eb9df69f5e00fd160251bac
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-05 19:31:23 -0500

    updated comments

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,10 +1,16 @@
 // Required dependencies
 const router = require('express').Router();
 const path = require('path');
+const fs = require('fs');
 // Helper method for generating unique ids
 const uuid = require('../helpers/uuid');
 const notes = require('../db/db.json');
-const fs = require('fs');
+
+// Setting readNotes as global variable to read db.json
+const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
+// Setting parsedNotes as global variable to parse readNotes into json format
+const parsedNotes = JSON.parse(readNotes);
+
 // Access to get db.json
 router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
@@ -18,14 +24,10 @@ router.get('/api/notes', (req, res) => {
         ...req.body,
         id: uuid(),
       };
-    // Read db.json file
-      const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
-    // Parsing readNotes into json format
-      const parsedNote = JSON.parse(readNotes);
     // Pushing parsedNote into a new note
-      parsedNote.push(newNote);
+      parsedNotes.push(newNote);
     // stringify parsedNote into proper json format
-      const newNoteString = JSON.stringify(parsedNote, null, 2);
+      const newNoteString = JSON.stringify(parsedNotes, null, 2);
     // Writing newNote to db.json file
       fs.writeFileSync(`./db/db.json`, newNoteString);
       const response = console.info({
@@ -40,8 +42,6 @@ router.get('/api/notes', (req, res) => {
 // Process to delete selected note with corresponding id
   router.delete('/api/notes/:id', (req, res) => {
     console.info('request received to delete selected note');
-    const readNotes = fs.readFileSync(`./db/db.json`, 'utf8');
-        const parsedNotes = JSON.parse(readNotes);
         const updatedNotes = parsedNotes.filter((notes) => notes.id !== req.params.id);
         // Update db.json after selected note has been deleted
         fs.writeFileSync('./db/db.json', JSON.stringify(updatedNotes, null, 2))
